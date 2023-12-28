5582023016dad498b37113e3f0a5d72da2954a36
HehdhHdhsj <singhsneha2505@gmail.com>
2023-12-28 16:35:50 +0530

    Initial deployment

---
+++ webserver.js
@@ -0,0 +1,44 @@
+const http = require('http')
+const fs = require('fs')
+const html =fs.readFileSync("./Template/index.html",'utf-8')
+const productsData = fs.readFileSync("./Data/products.txt", "utf-8")
+
+// creating server
+const server = http.createServer((req, res) => {
+    let path =req.url
+    
+    if ( path == '/' || path.toLocaleLowerCase() == '/home'){
+        res.writeHead(200)
+        res.end(html.replace("{{%CONTENT%}}", "Home"))
+    }
+    else if ( path.toLocaleLowerCase() == '/about'){
+        res.writeHead(200)
+        res.end(html)
+    }
+    else if ( path.toLocaleLowerCase() == '/contact'){
+        res.writeHead(200)
+        res.end(html)
+    }
+    else if ( path.toLocaleLowerCase() == '/products'){
+        res.writeHead(200, {'Content-Type': 'text/html'})
+        fs.readFile('./Data/products.json', 'utf8', (err, data) => {
+            res.end(html.replace("{{%CONTENT%}}", productsData))
+        })
+
+        
+    }
+    else{
+        res.writeHead(404)
+        res.end("Page not found")
+    }
+
+
+})  
+
+// starting server
+server.listen(8000,'127.0.0.1',() => {
+    console.log('Server started!!')
+})
+
+
+
