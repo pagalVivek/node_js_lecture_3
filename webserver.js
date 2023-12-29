2900b09ed86837896f773c9440d3e0b5b36425b5
Binduguptaa <bindeshgupta144@gmail.com>
2023-12-29 13:25:06 +0530

    updated code

--- webserver.js
+++ webserver.js
@@ -1,42 +1,39 @@
 const http = require('http')
 const fs = require('fs')
-const html =fs.readFileSync("./Template/index.html",'utf-8')
-const productsData = fs.readFileSync("./Data/products.txt", "utf-8")
+const html = fs.readFileSync("./Template/index.html", 'utf-8')
+const productsData = JSON.parse(fs.readFileSync('./Data/products.json', 'utf-8'));
 
 // creating server
 const server = http.createServer((req, res) => {
-    let path =req.url
-    
-    if ( path == '/' || path.toLocaleLowerCase() == '/home'){
+    let path = req.url
+
+    if (path == '/' || path.toLocaleLowerCase() == '/home') {
         res.writeHead(200)
         res.end(html.replace("{{%CONTENT%}}", "You are in Home page"))
     }
-    else if ( path.toLocaleLowerCase() == '/about'){
+    else if (path.toLocaleLowerCase() == '/about') {
         res.writeHead(200)
-        res.end(html.replace("{{%CONTENT%}}",'You are in About page'))
+        res.end(html.replace("{{%CONTENT%}}", 'You are in About page'))
     }
-    else if ( path.toLocaleLowerCase() == '/contact'){
+    else if (path.toLocaleLowerCase() == '/contact') {
         res.writeHead(200)
         res.end(html.replace("{{%CONTENT%}}", 'You are in Contact page'))
     }
-    else if ( path.toLocaleLowerCase() == '/products'){
-        res.writeHead(200, {'Content-Type': 'text/html'})
-        fs.readFile('./Data/products.json', 'utf8', (err, data) => {
-            res.end(html.replace("{{%CONTENT%}}", productsData))
-        })
-
-        
+    else if (path.toLocaleLowerCase() == '/products') {
+        res.writeHead(200, { 'Content-Type': 'application/json' })
+        res.end("You are in products page");
+        console.log(productsData);
     }
-    else{
+    else {
         res.writeHead(404)
         res.end("Page not found")
     }
 
 
-})  
+})
 
 // starting server
-server.listen(8000,'127.0.0.1',() => {
+server.listen(8000, '127.0.0.1', () => {
     console.log('Server started!!')
 })
 
